49fbfb571e970ab4eaee0104e3759de97e3799d9
Adam Mechaeil <mechaeil66@yahoo.com>
2025-08-28 16:24:59 +0530

    initial commit

---
+++ server/models/user.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  friends: {
+    type: Array,
+    default:[]
+  },
+  requestsReceived: {
+    type: Array,
+    default: [],
+  },
+  requestSent: {
+    type: Array,
+    default: [],
+  },
+});
+
+const User = model("User", userSchema);
+export default User;
